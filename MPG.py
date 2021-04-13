6a98f893901943b3aefb0c1b7762d19e7b037e90
Harris Toe <52926204+thatguytoe@users.noreply.github.com>
2021-04-13 07:58:30 -0400

    Add files via upload

---
+++ MPG.py
@@ -0,0 +1,17 @@
+'''
+    Harris Toe
+    ENTD200 1003 Spr19
+    Abaza, Ahmed
+    Week  4 
+    June 10, 2019
+'''
+'''
+a = MPG
+b = Miles Driven
+c = gas used per gallon
+'''
+a=36
+b=574
+c=5
+d=b/c
+print("MPG = ", d)
